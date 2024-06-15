c72ecdbe1702711e9ebdae6aeefe6480a7c01147
Lucas Silva <lucasvida2005@gmail.com>
2024-06-14 23:24:49 -0300

    Adding Switch Case and some fixes

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskList {
-    static int nextId = 0;
+    static int nextId = 1;
     static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void addTask(String taskName, String comments) {
@@ -41,45 +41,53 @@ public class TaskList {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
 
-        System.out.println("TASK MANAGER \n");
-        System.out.println("1. Add a new task");
-        System.out.println("2. Remove a task");
-        System.out.println("3. List all tasks");
-        System.out.println("4. Mark as done \n");
+        loop:
+        while (true) {
+            System.out.println("TASK MANAGER \n");
+            System.out.println("1. Add a new task");
+            System.out.println("2. Remove a task");
+            System.out.println("3. List all tasks");
+            System.out.println("4. Mark as done");
+            System.out.println("5. Terminate program \n");
 
-        System.out.print("Enter your choose: ");
-        int choose = Integer.parseInt(scr.nextLine());
+            System.out.print("Enter your choose: ");
+            int choose = Integer.parseInt(scr.nextLine());
 
-        switch (choose) {
-            case 1: {
-                System.out.println("Enter a name: ");
-                String taskName = scr.nextLine();
+            switch (choose) {
+                case 1: {
+                    System.out.println("Enter a name: ");
+                    String taskName = scr.nextLine();
 
-                System.out.println("Enter the comments: ");
-                String comments = scr.nextLine();
+                    System.out.println("Enter the comments: ");
+                    String comments = scr.nextLine();
 
-                addTask(taskName, comments);
-                break;
-            }
-            case 2: {
-                System.out.print("Enter the task ID: ");
-                int taskId = scr.nextInt();
+                    addTask(taskName, comments);
+                    break;
+                }
+                case 2: {
+                    System.out.print("Enter the task ID: ");
+                    int taskId = Integer.parseInt(scr.nextLine());
 
-                removeTask(taskId);
-                break;
-            }
-            case 3: {
-                System.out.println("Listing all tasks...");
+                    removeTask(taskId);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Listing all tasks...");
 
-                listTask();
-                break;
-            }
-            case 4: {
-                System.out.print("Enter the task that you want to mark as done: ");
-                int taskId = scr.nextInt();
+                    listTask();
+                    break;
+                }
+                case 4: {
+                    System.out.print("Enter the task that you want to mark as done: ");
+                    int taskId = Integer.parseInt(scr.nextLine());
 
-                markAsDone(taskId);
-                break;
+                    markAsDone(taskId);
+                    break;
+                }
+                case 5: {
+                    System.out.println("Terminating...");
+                    break loop;
+                }
             }
         }
     }
